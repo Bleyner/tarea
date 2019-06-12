4e5040b2607d7d0dc2e5bcea34b0154fc02b4109
Bleyner <50389568+Bleyner@users.noreply.github.com>
2019-06-11 19:43:54 -0500

    Add files via upload

---
+++ tarea.js
@@ -0,0 +1,28 @@
+// var titulo1;
+// var titulo2;
+// var titulo3;
+// var texto1;
+// var texto2;
+// var texto3;
+
+//  titulo1 = prompt("escriba un titulo", " ");
+//  	texto1 = prompt("escriba un texto", " ");
+//  titulo2 = prompt("escriba un titulo", " ");
+//  	 texto2 = prompt("escriba un texto", " ");
+//  titulo3 = prompt("escriba un titulo", " ");
+// 	texto3 = prompt("escriba un texto", " ");
+
+// document.getElementById('tit1').innerHTML= titulo1;
+// document.getElementById('tit2').innerHTML= titulo2;
+// document.getElementById('tit3').innerHTML= titulo3;
+
+// document.getElementById('tex1').innerHTML= texto1;
+// document.getElementById('tex2').innerHTML= texto2;
+// document.getElementById('tex3').innerHTML= texto3;
+
+
+document.getElementById('tit1').innerHTML='Cuidados para sus mascotas'
+document.getElementById('tex1').innerHTML='Les ofrecemos el cuidado para sus mascotas las 24 horas...'
+
+document.getElementById('tit2').innerHTML='Productos para sus mascotas '
+document.getElementById('tex2').innerHTML='Le ofrecemos productos de muy buna calida para la salud de sus mascotas...'
